08c2c3c19ba18aa9adfad39b4a4ec129a6db0579
Tim <TimothyMLOwens@gmail.com>
2025-10-05 13:31:25 -0700

    Skeleton nav and pages

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Navbar from "src/components/Navbar.js";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -19,6 +19,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Navbar />
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
       </body>
