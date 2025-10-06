1b90f9e7cb3336a9ee8e828394d7b12c026f8bc9
Tim <TimothyMLOwens@gmail.com>
2025-10-06 15:34:31 -0700

    Barebones wireframing of content. No styling

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,27 @@
 export default function Home() {
   return (
     <div>
-      <h1>Landing Page</h1>
-      <p>Add user login, description of the site, etc</p>
+      <div>
+        <h1>The Hero Section</h1>
+        <p>Content to go here:</p>
+        <li>Headline (what the product/service does)</li>
+        <li>Subheadline (optional extra context)</li>
+        <li>Primary CTA button (e.g., “Get Started” or “Login”)</li>
+        <li>Optional visual: illustration, product screenshot, or hero image/video</li>
+      </div>
+      <div>
+        <h1>Product Features</h1>
+        <li>Highlight product benefits</li>
+        <li>Usually Columns or Cards</li>
+        <li>Secondary CTA can/may appear here as reinforcement</li>
+      </div>
+      <div>
+        <h1>Social Proof or Testimonials</h1>
+        <p>This section may be excluded at the start but putting this in for the wireframe</p>
+        <li>User Testimonials</li>
+        <li>Trust Logos(partners, press mentions)</li>
+        <li>Optional Metrics</li>
+      </div>
     </div>
   );
 }
