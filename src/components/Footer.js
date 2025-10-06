5f07e1203618fd970b06886422bd3fa9834016d7
Tim <TimothyMLOwens@gmail.com>
2025-10-06 15:38:51 -0700

    Footer with links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,13 @@
+import Link from "next/link";
+
 export default function Footer() {
-    <footer>Placeholder Text</footer>
+    <footer>
+        <Link href="/">Dashboard</Link>
+        <Link href="/habits">Habits</Link>
+        <Link href="/quests">Quests</Link>
+        <Link href="/skills">Skills</Link>
+        <Link href="/journal">Journal</Link>
+        <Link href="/Inventory">Inventory</Link>
+        <p>Placeholder Text - TBD</p>
+    </footer>
 }
